e462d41fb0c95cfbce8d8a6d51622d12186356a6
Ronaldo <ronaldokun@gmail.com>
2020-02-26 12:22:46 -0300

    Finished Scraping

--- 00_scraper.py
+++ 00_scraper.py
@@ -23,41 +23,43 @@
 # create initially a toy recommendation engine / wine classifier and pricing tool.
 
 import scrapy
-from bs4 import BeautifulSoup as soup
 from fire import Fire
 from nbdev.showdoc import *
 from scrapy.crawler import CrawlerProcess
-from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from functools import partial
+from bs4 import BeautifulSoup as soup
 
+CATALOG = "//article"
+NEXT = "/html/body/div[6]/div/div[2]/div[2]/div/div[4]/div"
 prefix = "https://wine.com.br"
 url_short = "https://www.wine.com.br/browse.ep?cID=100851&exibirEsgotados=true&listagem=horizontal&sorter=featuredProducts-desc&filters=cVINHOS"
-# -
-
-CATALOG = "//article/div[2]"
-NEXT = "/html/body/div[6]/div/div[2]/div[2]/div/div[4]/div"
-
-link_extract = LxmlLinkExtractor(restrict_text="Próxima >>")
+url_next = "https://www.wine.com.br/browse.ep?cID=100851&exibirEsgotados=true&pn={page}&listagem=horizontal&sorter=featuredProducts-desc&filters=cVINHOS"
 
 
-# Create the Spider class
-class WineSpider(scrapy.Spider):
-    name = "winespider"
+class CatalogClassic(scrapy.Spider):
+    name = "catalog_classic"
+    #url = url_short
+    i=1
 
     def start_requests(self):
         yield scrapy.Request(url=url_short, callback=self.parse_page)
 
-    def parse_page(self, response):
+    def parse_page(self, response, count=i):
         wine_list = response.xpath(CATALOG)
         for block in wine_list:
             yield {
                 "wine": block.css('div > a::attr("title")').get(),
                 "link": prefix + block.css('div > a::attr("href")').get(),
+                "país": block.xpath('div[2]/div[1]/div/span').get()
             }
-
-        next_page = response.xpath(f"//a[./text()='Próxima >>']/@href").get()
-        # next_page = response.xpath("//a[contains(text(), 'Próxima')]/@href").get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse_page)
+        #/html/body/div[5]/div/div[2]/div[2]/div/div[3]/ul/li[1]/article/div[2]/div[2]/div[1]/div/span
+        #next_page = response.xpath(f"//a[./text()='Próxima >>']/@href").get()
+        count +=1
+        next_page = url_next.format(page=count)
+        parse_next = partial(self.parse_page, count=count)
+        if count <= 431:
+            yield response.follow(next_page, parse_next)
+        
 
     # Second parsing method
     def parse_pages(self, response):
@@ -68,18 +70,40 @@ class WineSpider(scrapy.Spider):
         dc_dict[crs_title_ext] = ch_titles_ext
 
 
+#export
+class CatalogFaster(scrapy.Spider):
+    name = "catalog"
+    start_urls = [url_short] + [url_next.format(page=i) for i in range(2, 434)]
+    
+
+    def parse(self, response):
+        wine_list = response.xpath(CATALOG)
+        for block in wine_list:
+            yield {
+                "wine": block.css('div > a::attr("title")').get(),
+                "link": prefix + block.css('a::attr("href")').get(),
+                "pontuação": block.xpath('div[2]/div[2]/div[4]/div/div/div/div/span/text()').get(),
+                "avaliações": block.xpath('div[2]/div[2]/div[4]/div/a/text()').get(),
+                "volume": block.xpath('div[2]/div[2]/div[3]/text()').get()
+                
+            }
+    
+    # Second parsing method
+    def parse_pages(self, response):
+        crs_title = response.xpath('//h1[contains(@class,"title")]/text()')
+        crs_title_ext = crs_title.extract_first().strip()
+        ch_titles = response.css("p.course__description::text")
+        ch_titles_ext = [t.strip() for t in ch_titles.extract()]
+        dc_dict[crs_title_ext] = ch_titles_ext
+
+
 # +
-# Initialize the dictionary **outside** of the Spider class
-dc_dict = dict()
-
-
 def crawl():
     # Run the Spider
     process = CrawlerProcess()
-    process.crawl(WineSpider)
+    process.crawl(CatalogFaster)
     process.start()
 
 
 if __name__ == "__main__":
     Fire(crawl)
-# -
---
+++ 01_wine_extractor.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.3
+#   kernelspec:
+#     display_name: wino
+#     language: python
+#     name: wino
+# ---
+
+# +
+import pandas as pd
+from pathlib import Path
+from bs4 import BeautifulSoup as soup
+from pprint import pprint as pp
+from itertools import tee
+import scrapy
+from fire import Fire
+from scrapy.crawler import CrawlerProcess
+DATA = Path.cwd() / 'data'
+PAGE = '.gr__wine_com_br'
+FICHA = '.TechnicalDetails'
+URL = 'https://www.wine.com.br/vinhos/casillero-reserva-limited-edition-cabernet-sauvignon/prod23130.html'
+
+df = pd.read_csv(DATA / 'wines.csv')
+
+TAG = '''<div class="TechnicalDetails">\n<div class="container visible-xs">\n<div class="row somelier">\n<div class="col-xs-12">
+         <h2 class="somelier__title">Comentário do Sommelier</h2>
+         <div class="somelier__description">\nEdição especial para o Halloween, esse Cabernet Sauvignon além de apresentar um rótulo ousado e divertido, também traz a elegância de um belo exemplar Concha Y Toro.O Casillero Del Diablo é um tinto frutado, equilibrado e saboroso! Uma edição limitada!\n</div>\n</div>\n
+         <div class="col-xs-12 content-space">\n</div>\n</div>\n</div>\n<div class="container">\n
+         <div class="row">\n<div class="col-xs-12 col-md-8 col-md-offset-4 col-sm-7 col-sm-offset-5">\n
+         <h2 class="TechnicalDetails-title">\nFicha Técnica\n </h2>\n</div>\n</div>\n<div class="TechnicalDetails-image">\n
+         <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="30065b171190d5cba9ad9c02-|49"></script><img title="Casillero Reserva Limited Edition Cabernet Sauvignon" alt="Casillero Reserva Limited Edition Cabernet Sauvignon" src="https://www.wine.com.br/cdn-cgi/image/f=auto,h=900,q=100/assets-images/produtos/23130-03.png" onerror="this.parentElement.parentElement.classList.add(\'js-wine-kit\')">\n</div>\n
+         <div class="row">\n<div class="col-xs-12 col-md-8 col-md-offset-4 col-sm-7 col-sm-offset-5">\n
+         <div class="row">\n<div class="col-xs-12 col-sm-6">\n<div class="row">\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--grape">\n
+         <div class="Left">\n<i class="Icon Icon--grapes"></i>\n</div>\n
+         <div class="Right">\n<dt>Tinto</dt>\n<dd>Cabernet Sauvignon (100.00%)</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--location">\n
+         <div class="Left">\n<i class="Icon Icon--location"></i>\n</div>\n
+         <div class="Right">\n<dt>Chile</dt>\n<dd>Valle Central </dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--winery">\n
+         <div class="Left">\n<i class="Icon Icon--winery"></i>\n</div>\n
+         <div class="Right">\n<dt>Vinícola</dt>\n<dd>Concha y Toro</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--alcoholic_strength">\n
+         <div class="Left">\n<i class="Icon Icon--wine-and-cup"></i>\n</div>\n
+         <div class="Right">\n<dt>Teor Alcoólico</dt>\n<dd>13.50% ABV</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--ageing">\n
+         <div class="Left">\n<i class="Icon Icon--ageing"></i>\n</div>\n
+         <div class="Right">\n<dt>Amadurecimento</dt>\n<dd>Em barricas de carvalho.</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--harvest">\n
+         <div class="Left">\n<i class="Icon Icon--harvest"></i>\n</div>\n
+         <div class="Right">\n<dt>Safra</dt>\n<dd>2017</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--classification">\n
+         <div class="Left">\n<i class="Icon Icon--classification"></i>\n</div>\n
+         <div class="Right">\n<dt>Classificação</dt>\n<dd>Seco</dd>\n</div>\n</div>\n</div>\n</div>\n
+         <div class="col-xs-12  col-sm-6">\n<div class="row">\n<div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--appearance">\n
+         <div class="Left">\n<i class="Icon Icon--appearance"></i>\n</div>\n
+         <div class="Right">\n<dt>Visual</dt>\n <dd>Rubi intenso</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--aroma">\n
+         <div class="Left">\n<i class="Icon Icon--aroma"></i>\n</div>\n
+         <div class="Right">\n<dhttps://www.wine.com.br/vinhos/casillero-reserva-limited-edition-cabernet-sauvignon/prod23130.htmlt>Olfativo</dt>\n<dd>Cassis, cereja, ameixa e notas de tostado</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--taste">\n
+         <div class="Left">\n<i class="Icon Icon--taste"></i>\n</div>\n
+         <div class="Right">\n<dt>Gustativo</dt>\n<dd>Corpo médio, taninos sedosos e final longo e frutado.</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--temperature">\n
+         <div class="Left">\n<i class="Icon Icon--temperature"></i>\n</div>\n
+         <div class="Right">\n<dt>Temperatura de serviço</dt>\n<dd>15 °C</dd>\n</div>\n</div>\n
+         <div class="col-xs-8 col-sm-12 TechnicalDetails-description TechnicalDetails-description--temperature">\n
+         <div class="Left">\n<i class="Icon Icon--ageing_potential"></i>\n</div>\n
+         <div class="Right">\n<dt>Potencial de guarda</dt>\n<dd>4 anos</dd>\n</div>\n</div>\n</div>\n</div>\n</div>\n</div>\n</div>\n
+         <div class="row">\n<div class="col-xs-12 col-md-8 col-md-offset-4 col-sm-7 col-sm-offset-5">\n
+         <article class="TechnicalDetails-matching">\n<dt>Harmonização</dt>\n<dd>Carnes vermelhas, pratos condimentados e queijos envelhecidos como Gruyère e azuis.</dd>\n</article>\n</div>\n</div>\n</div>\n</div>
+      '''
+
+# +
+TIPOS = {'Branco', 'Espumante', 'Frisante', 'Licoroso', 'Rosé', 'Tinto'}
+PAISES = {'África do Sul', 'Alemanha', 'Argentina', 'Austrália', 'Brasil', 'Chile'
+          'China', 'Espanha', 'Estados Unidos', 'França', 'Hungria', 'Itália', 'Líbano'
+          'Nova Zelândia', 'Portugal', 'Uruguai', 'Grécia', 'Marrocos'}
+
+KEYS = {'Vinícola', 'Teor_Alcoólico', 'Amadurecimento', 'Safra','Classificação', 'Visual', 
+        'Olfativo', 'Gustativo', 'Temperatura', 'Potencial_Guarda', 'Decantação''Harmonização'}
+
+# -
+
+#export
+class FichaTecnica(scrapy.Spider):
+    
+    name = "ficha_tecnica"
+    
+    start_urls = df['link'].to_list()
+    
+
+    def parse(self, response):
+        page = response.xpath('/html').get()
+        yield self.ficha_tecnica(page)
+
+    # Second parsing method
+    def ficha_tecnica(self, tag):
+        tag = soup(tag, 'lxml')
+        result = {}
+        key = []
+        val = []
+        
+        v = tag.find(class_="PageHeader-title")
+        key.append("title")
+        val.append(v.string if v else '')
+        
+        v = tag.find(class_="somelier__description")
+        key.append('somelier')
+        val.append(v.string.strip() if v else '')
+        
+        precos = tag.find_all(class_='Price-raw')        
+        
+        if len(precos) > 2:
+            precos = [float(p.string) for p in precos]
+            key.append('Preço_Sócio')
+            val.append(min(precos))
+            key.append('Preço_Normal')
+            val.append(max(precos))
+            
+        
+        keys = [t.string for t in tag.find_all('dt')]
+        vals = [t.string for t in tag.find_all('dd')]
+        
+               
+        for k,v in zip(keys, vals):
+            if k in TIPOS:
+                key.append('tipo')
+                val.append(k)
+            elif k in PAISES:
+                key.append('origem')
+                val.append(f'{k}-{v}')
+            else:
+                key.append(k)
+                val.append(v)
+           
+        
+        return dict(zip(key, val))
+        #try:
+        #    ficha.remove("Ficha Técnica")
+        #except:
+        #    pass
+        #field, description =  tee(ficha, 2)
+        #field, description = list(field)[::2], list(description)[1::2]
+        #return {f:d for f,d in zip(field, description)}
+
+if __name__ == "__main__":
+    
+    process = CrawlerProcess()
+    process.crawl(FichaTecnica)
+    process.start()
+
+tag = """<div class="ClubPrice-value">
+<span>Sócio:</span>
+<span>
+<span class="Price is-loading" v-price="v-price">
+<span class="Price-raw">126.90</span>
+<span class="Price-formatted"></span>
+</span>
+</span>"""
+
+
+tag = soup(tag, 'lxml')
+
+tag.find('span', class_='Price-raw')
+
+
