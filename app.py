e86127f1d1633e22c9c3e6e20deeeabdb9eea381
Ronaldo <ronaldokun@gmail.com>
2020-04-02 18:26:05 -0300

    Fixed Bug

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ st.subheader('Conjunto de Dados')
 if st.checkbox("Mostrar Dados?"):
     st.dataframe(DATA)
 
-variables = set(NUM.keys())
+variables = set(NUM)
 variables.remove('Custo')
 
 st.markdown("""
